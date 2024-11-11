0b0d70a957ccb54ed651411d7018143f5853a48f
Dhruv Sharma <98601824+dhruv30sharma13@users.noreply.github.com>
2024-11-11 12:53:10 +0530

    Add files via upload
    Missed adding necessary import file
    Please consider

---
+++ model.py
@@ -0,0 +1,12 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import torch
+
+torch.random.manual_seed(0)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3.5-mini-instruct", 
+    device_map="cuda", 
+    torch_dtype="auto", 
+    trust_remote_code=True, 
+)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3.5-mini-instruct")
